cf0b7d12da6feeaf7dd260849dca679627c49493
DraKenCodeZ <drakencodez1337@gmail.com>
2022-04-10 18:35:33 +0530

    Idk

--- main.py
+++ main.py
@@ -5,23 +5,6 @@ app = Flask(__name__,template_folder="./templates")
 @app.route("/")
 def home():
 	return render_template("index.html")
-
-@app.route("/github")
-def githubig():
-	headers = {"Content-type":"text/html"}
-	return make_response(render_template("github.html"),200,headers)
 	
-@app.route("/mail")
-def mail():
-	return render_template("mail.html")
-	
-@app.route("/discord")
-def discord():
-	return render_template("discord.html")
-	
-@app.route("/youtube")
-def youtube():
-	return render_template("youtube.html")
-
 if __name__ == "__main__":
 	app.run(debug=True)
