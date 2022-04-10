8e130b36ed2535c20229fa6dc36536cd7eda9e90
DraKenCodeZ <drakencodez1337@gmail.com>
2022-04-10 18:38:07 +0530

    Idk

--- main.py
+++ main.py
@@ -6,21 +6,5 @@ app = Flask(__name__,template_folder="./templates")
 def home():
 	return render_template("index.html")
 	
-<<<<<<< HEAD
 if __name__ == "__main__":
 	app.run(debug=True)
-=======
-@app.route("/mail")
-def mail():
-	return render_template("mail.html")
-	
-@app.route("/discord")
-def discord():
-	return render_template("discord.html")
-	
-@app.route("/youtube")
-def youtube():
-	return render_template("youtube.html")
-
-app.run(debug=True)
->>>>>>> refs/remotes/origin/main
